250c60cf8401ed955c246a4fdb941b0a91be9ccf
Yuvraj <yuvraj.r0810@gmail.com>
2025-12-26 12:52:52 +0530

    Tic_Tac_Toe: Board with Square components props

--- Tic_Tac_Toe/src/App.jsx
+++ Tic_Tac_Toe/src/App.jsx
@@ -1,37 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 
-
-
-
-function App() {
-  const [count, setCount] = useState(0)
-
-function maingame(){
-setCount(count + 1)
+function Square({value}){
   return(
-<>
-  <h1> we are oppeing an modal</h1> 
-</>
-
+  <button className="square">{value}</button>
   )
 };
 
+function App() {
 
   return (
     <>
-    <h1>Tic Toe Game</h1>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <br />
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <br />
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
     </>
   )
 };
