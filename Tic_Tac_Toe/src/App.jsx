0552ce136d2ddfa49247bc7036d61135a632433e
Yuvraj <yuvraj.r0810@gmail.com>
2025-12-26 16:44:51 +0530

    Tic_Tac_Toe: V1 completed feat : winner decide turn based complete

--- Tic_Tac_Toe/src/App.jsx
+++ Tic_Tac_Toe/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   const [xisNext, setxisnext] = useState(true);
   
   function handleClick(i){
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares) ){
     return;
   }
     const nextSquares = squares.slice();
@@ -34,9 +34,19 @@ function App() {
     setxisnext(!xisNext);
   }
 
+    const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xisNext? 'X' : 'O');
+  }
+  
 
   return (
     <>
+    <div className='stytling'>
+    <div className="status">{status}</div>
      <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -52,8 +62,31 @@ function App() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+</div>
     </>
   );
 };
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+
 
 export default App
